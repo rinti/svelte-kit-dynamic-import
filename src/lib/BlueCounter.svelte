f650d5735b80e204921140a088fcc71065a53956
Andreas Bernacca <hello@rinti.se>
2021-04-11 18:12:59 +0200

    Initial

---
+++ src/lib/BlueCounter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let text;
+	let count = 0;
+
+	const increment = (): void => {
+		count += 1;
+	};
+</script>
+
+<button on:click={increment}>
+    i am {text}, clicks: {count}
+</button>
+
+<style>
+	button {
+		font-family: inherit;
+		font-size: inherit;
+		padding: 1em 2em;
+		color: blue;
+		background-color: rgba(0, 62, 255, 0.1);
+		border-radius: 2em;
+		border: 2px solid blue;
+		outline: none;
+		font-variant-numeric: tabular-nums;
+	}
+
+	button:hover {
+		border: 3px solid blue;
+	}
+
+	button:active {
+		background-color: rgba(0, 62, 255, 0.2);
+	}
+</style>
---
+++ src/routes/[slug].svelte
@@ -0,0 +1,27 @@
+<script context="module">
+	export async function load({ page, fetch, session, context }) {
+        let counter;
+
+		if (page.path === '/blue-counter') {
+			counter = (await import('$lib/BlueCounter.svelte')).default;
+		} else {
+			counter = (await import('$lib/Counter.svelte')).default;
+		}
+
+		return { props: { counter, text: page.path } };
+	}
+</script>
+
+<script lang="ts">
+	export let counter;
+	export let text;
+
+	import '../app.css';
+</script>
+
+<main>
+    <a href="/blue-counter">Blue</a>
+    <a href="/red-counter">Red</a>
+
+    <svelte:component this="{counter}" text="{text}" />
+</main>
